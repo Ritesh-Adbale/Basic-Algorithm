271fccbba6616862923bf53092aaefbef971a10e
Ritesh Adbale <ritesh.adbale@gmail.com>
2022-04-05 11:26:36 +0530

    return true if one of them is 30 or if their sum is 30.

---
+++ Basic Algorithm/Two.cs
@@ -0,0 +1,33 @@
+﻿// Write a C# Sharp program to check two given integers, and return true if one of them is 30 or if their sum is 30. 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Algorithm
+{
+    class Two
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1st No: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 2nd No: ");
+            int b = int.Parse(Console.ReadLine());
+
+            bool add()
+            {
+                return a == 30 || b == 30 || a + b == 30 ? true : false;
+            }
+
+            Console.WriteLine(add());
+
+
+            Console.ReadKey();
+        }
+    }
+}
